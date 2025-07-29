6603e905c872998c29453b41f56c0d0433038135
Mahi soni <167469698+639251@users.noreply.github.com>
2025-07-29 18:24:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -131,24 +131,3 @@ function loadMessages() {
 window.onload = function () {
   loadMessages();
 };
-function getBotReply(userMessage) {
-    const msg = userMessage.toLowerCase();
-
-    if (msg.includes("hello") || msg.includes("hi")) {
-        return "Hello! How can I help you?";
-    } else if (msg.includes("your name")) {
-        return "I'm ChatBot 🤖 , your name";
-    } else if (msg.includes("where are you from") || msg.includes("kha s ho")) {
-        return "I'm from Lucknow and you 😊";
-    } else if (msg.includes("my name is ...")) {
-        return "ok cool!";
-    } else if (msg.includes("how are you")) {
-        return "I'm good! Thanks for asking 😊";
-    } else if (msg.includes("bye")) {
-        return "Goodbye! Take care.";
-    } else if (msg.includes("who makes you")) {
-        return "Mahi Soni! Thanks for asking 😊";
-    } else {
-        return "I'm still learning! Can you try something else?";
-    }
-}
